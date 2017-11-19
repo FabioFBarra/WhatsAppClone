64f8f8c519af9cd559704754fe7826c698917c68
fabio <fabiofigueiredovr@bol.com.br>
2017-11-19 16:26:55 -0200

    Adicionado listener para leitura de dados dinamica de contatos

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/fragment/ContatosFragment.java
@@ -9,10 +9,18 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
 import br.com.whatsappandroid.cursoandroid.whatsapp.activity.MainActivity;
+import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import br.com.whatsappandroid.cursoandroid.whatsapp.helper.Preferencias;
+import br.com.whatsappandroid.cursoandroid.whatsapp.modelo.Contato;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +30,7 @@ public class ContatosFragment extends Fragment {
     private ListView listView;
     private ArrayAdapter arrayAdapter;
     private ArrayList<String> contatos;
+    private DatabaseReference databaseReference;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -35,9 +44,6 @@ public class ContatosFragment extends Fragment {
         //Objetos
 
         contatos = new ArrayList<>();
-        contatos.add("teste");
-        contatos.add("teste2");
-
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_contatos, container, false);
@@ -46,9 +52,39 @@ public class ContatosFragment extends Fragment {
         //Montando listview
         listView = (ListView) view.findViewById(R.id.lv_contatos);
 
-        arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, contatos);
+        arrayAdapter = new ArrayAdapter(getActivity(), R.layout.lista_contato, contatos);
 
         listView.setAdapter(arrayAdapter);
+
+        //recuperar dados do firebase
+        Preferencias preferencias = new Preferencias(getActivity());
+        String usuarioLogado = preferencias.getIdentificador();
+
+        databaseReference = ConfiguracaoFirebase.getFirebase().child("contatos").child(usuarioLogado);
+
+        //listener para recuperar contatos
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Limpar lista
+                contatos.clear();
+
+                //listar contatos
+                for(DataSnapshot dados: dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class);
+                    contatos.add(contato.getNome());
+                }
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return view;
     }
